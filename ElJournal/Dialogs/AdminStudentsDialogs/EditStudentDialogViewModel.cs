044b2724935e5528fca501882661d707da1aa1a5
Vlad Chekanov <vampe@DESKTOP-RFVDE49>
2021-04-27 13:51:22 +0300

    Сверстан диалог редактирования учащегося и на все диалоговые окна добавлены стили

--- ElJournal/Dialogs/AdminStudentsDialogs/EditStudentDialogViewModel.cs
+++ ElJournal/Dialogs/AdminStudentsDialogs/EditStudentDialogViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ElJournal.Other;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ElJournal.Dialogs.AdminStudentsDialogs
 {
-    class EditStudentDialogViewModel
+    class EditStudentDialogViewModel: BaseViewModel
     {
+        
     }
 }
