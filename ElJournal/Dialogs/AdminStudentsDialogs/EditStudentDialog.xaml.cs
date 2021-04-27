377a8cbc80e4d82bfa8babd5e4e7e1feeb8a30de
Vlad Chekanov <vampe@DESKTOP-RFVDE49>
2021-04-27 12:54:34 +0300

    Начана верстка редактирования учащегося

---
+++ ElJournal/Dialogs/AdminStudentsDialogs/EditStudentDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElJournal.Dialogs.AdminStudentsDialogs
+{
+    /// <summary>
+    /// Логика взаимодействия для EditStudentDialog.xaml
+    /// </summary>
+    public partial class EditStudentDialog : Window
+    {
+        public EditStudentDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
