73a959530dd220a897bb53e7e0af2ef1b6807af6
Vlad Chekanov <vampe@DESKTOP-RFVDE49>
2021-04-29 00:29:51 +0300

    Сверстано и готово диалоговое окно добавления предмета

--- ElJournal/Dialogs/AdminSubjectsDialogs/AddSubjectDialogViewModel.cs
+++ ElJournal/Dialogs/AdminSubjectsDialogs/AddSubjectDialogViewModel.cs
@@ -2,10 +2,60 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace ElJournal.Dialogs.AdminSubjectsDialogs
 {
     class AddSubjectDialogViewModel: BaseViewModel
     {
+        #region Поля
+
+        private string _shortName;
+        private string _name;
+        private DelegateCommand _addSubject;
+
+        #endregion
+
+        #region Свойства
+
+        public string ShortName
+        {
+            get => _shortName;
+            set
+            {
+                _shortName = value;
+                OnPropertyChanged(nameof(IsAddActive));
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(IsAddActive));
+            }
+        }
+
+        public bool IsAddActive => !string.IsNullOrWhiteSpace(_shortName) && !string.IsNullOrWhiteSpace(_name);
+
+        #endregion
+
+        #region Команды
+
+        public DelegateCommand AddSubject
+        {
+            get
+            {
+                return _addSubject = new DelegateCommand((arg) =>
+                {
+                    var window = (Window)arg;
+                    window.DialogResult = true;
+                });
+            }
+        }
+
+        #endregion
     }
 }
