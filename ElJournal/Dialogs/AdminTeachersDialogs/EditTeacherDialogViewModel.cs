9b9418d6eaf75dafc165bde29621935c6c6134ad
Vlad Chekanov <vampe@DESKTOP-RFVDE49>
2021-04-28 16:02:06 +0300

    Написан ViewModel для редактирования информации о преподавателе

--- ElJournal/Dialogs/AdminTeachersDialogs/EditTeacherDialogViewModel.cs
+++ ElJournal/Dialogs/AdminTeachersDialogs/EditTeacherDialogViewModel.cs
@@ -1,12 +1,102 @@
-﻿using ElJournal.Other;
+﻿using ElJournal.Models;
+using ElJournal.Other;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 
 namespace ElJournal.Dialogs.AdminTeachersDialogs
 {
-    class EditTeacherDialogViewModel: BaseViewModel
+    class EditTeacherDialogViewModel : BaseViewModel
     {
+        #region Поля
 
+        private string _ID;
+        private string _newFirstName;
+        private string _newLastName;
+        private string _newPatronymic;
+        private DelegateCommand _editTeacher;
+
+        #endregion
+
+        #region Свойства
+
+        public string ID
+        {
+            get => _ID;
+            set
+            {
+                _ID = value;
+                OnPropertyChanged(nameof(IsEditActive);
+            }
+        }
+
+        public string NewFirstName
+        {
+            get => _newFirstName;
+            set
+            {
+                _newFirstName = value;
+                OnPropertyChanged(nameof(IsEditActive));
+            }
+        }
+
+        public string NewLastName
+        {
+            get => _newLastName;
+            set
+            {
+                _newLastName = value;
+                OnPropertyChanged(nameof(IsEditActive));
+            }
+        }
+
+        public string NewPatrnomyic
+        {
+            get => _newPatronymic;
+            set
+            {
+                _newPatronymic = value;
+                OnPropertyChanged(nameof(IsEditActive));
+            }
+        }
+
+        public bool IsEditActive => !string.IsNullOrWhiteSpace(_ID) && !string.IsNullOrWhiteSpace(_newFirstName)
+            && !string.IsNullOrWhiteSpace(_newLastName) && !string.IsNullOrWhiteSpace(_newPatronymic);
+
+        #endregion
+
+        #region Команды
+
+        public DelegateCommand EditTeacher
+        {
+            get
+            {
+                return _editTeacher ??= new DelegateCommand((arg) =>
+                {
+                    if (Validation.StringToIntParse(_ID))
+                    {
+                        var teachermodel = new TeacherModel();
+                        if (teachermodel.GetItemByID(int.Parse(_ID)) != null)
+                        {
+                            var window = (Window)arg;
+                            window.DialogResult = true;
+                        }
+                        else
+                        {
+                            var err = new ErrorDialog(Validation.UserNotFoundError);
+                            err.ShowDialog();
+                        }
+                    }
+                    else
+                    {
+                        var err = new ErrorDialog(Validation.ImpossibleToParseError);
+                        err.ShowDialog();
+                    }
+                });
+            }
+        }
+
+        #endregion
     }
 }
