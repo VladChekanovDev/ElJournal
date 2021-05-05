d074ac4fead4c6ea81498a1eccd975c9e3b0b8f2
Vlad Chekanov <vampe@DESKTOP-RFVDE49>
2021-05-06 01:09:12 +0300

    Начана версттка учительской панели

---
+++ ElJournal/ViewModels/TeacherControlViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ElJournal.Other;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElJournal.ViewModels
+{
+    class TeacherControlViewModel: BaseViewModel
+    {
+        #region Поля
+
+
+
+        #endregion
+
+        #region Конструктор
+
+
+
+        #endregion
+
+        #region Свойства
+
+
+
+        #endregion
+
+        #region Команды
+
+
+
+        #endregion
+    }
+}
